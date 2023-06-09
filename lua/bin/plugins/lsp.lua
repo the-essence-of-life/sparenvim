a88816c105af242cccb426d365a6bf3b4667688b
the-essence-of-life <2193714201@qq.com>
2023-06-09 22:51:57 +0800

    v4.8 (changed:/lua/bin/lsp/mason-lspconfig.lua) Deleted! (bug) fixed.

--- lua/bin/plugins/lsp.lua
+++ lua/bin/plugins/lsp.lua
@@ -1,77 +1,78 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate", -- :MasonUpdate updates registry contents
-    dependencies = {
-      -- {"williamboman/mason-lspconfig.nvim", config = true},
-      {"neovim/nvim-lspconfig", config = true},
-      -- {"glepnir/lspsaga.nvim", config = true},
-      -- "jose-elias-alvarez/null-ls.nvim",
-    },
-    config = function()
-      require('bin.plugins.lsp.mason-lspconfig')
-      require('bin.plugins.lsp.lsp')
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      'neovim/nvim-lspconfig',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      "amarakon/nvim-cmp-buffer-lines",
-      'hrsh7th/cmp-path',
-      -- 'hrsh7th/cmp-cmdline',
-      -- 'hrsh7th/nvim-cmp',
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-      'onsails/lspkind.nvim',
-      'rafamadriz/friendly-snippets',
-    },
-    config = function()
-      require('bin.plugins.lsp.cmp')
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-    },
-    init = function ()
-      require("bin.plugins.init.lsp")
-    end,
-    config = function()
-      require('bin.plugins.lsp.lsp')
-    end,
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    -- dependencies = {
-      -- 'jose-elias-alvarez/typescript.nvim',
-      -- "neovim/nvim-lspconfig",
-    -- },
-    config = function()
-      require("bin.plugins.lsp.null-ls")
-    end,
-    requires = { "nvim-lua/plenary.nvim" },
-  },
-  -- {
-  --   'jose-elias-alvarez/typescript.nvim',
-  --   ft = {
-  --     'tsx',
-  --     'ts',
-  --     'vue',
-  --     'jsx',
-  --   },
-  --   config = function ()
-  --     require("bin.plugins.lsp.typescript")
-  --   end
-  -- },
-  -- {
-    --   "jose-elias-alvarez/null-ls.nvim",
-    --   dependencies = {
-      --     "jay-babu/mason-null-ls.nvim",
-      --   },
-      --   config = require('bin.plugins.lsp.null-ls')
-      -- },
-    }
+	{
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+		dependencies = {
+			-- {"williamboman/mason-lspconfig.nvim", config = true},
+			{ "neovim/nvim-lspconfig" },
+			-- {"glepnir/lspsaga.nvim", config = true},
+			-- "jose-elias-alvarez/null-ls.nvim",
+		},
+		config = function()
+			require("bin.plugins.lsp.lsp")
+		end,
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		version = "<CurrentMajor>.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		build = "make install_jsregexp",
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"amarakon/nvim-cmp-buffer-lines",
+			"hrsh7th/cmp-path",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"onsails/lspkind.nvim",
+			"rafamadriz/friendly-snippets",
+		},
+		config = function()
+			require("bin.plugins.lsp.cmp")
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+		},
+		-- config = function()
+		-- 	require("bin.plugins.lsp.lsp")
+		-- end,
+	},
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		-- dependencies = {
+		-- 'jose-elias-alvarez/typescript.nvim',
+		-- "neovim/nvim-lspconfig",
+		-- },
+		config = function()
+			require("bin.plugins.lsp.null-ls")
+		end,
+		requires = { "nvim-lua/plenary.nvim" },
+	},
+	-- {
+	--   'jose-elias-alvarez/typescript.nvim',
+	--   ft = {
+	--     'tsx',
+	--     'ts',
+	--     'vue',
+	--     'jsx',
+	--   },
+	--   config = function ()
+	--     require("bin.plugins.lsp.typescript")
+	--   end
+	-- },
+	-- {
+	--   "jose-elias-alvarez/null-ls.nvim",
+	--   dependencies = {
+	--     "jay-babu/mason-null-ls.nvim",
+	--   },
+	--   config = require('bin.plugins.lsp.null-ls')
+	-- },
+}
--- lua/bin/plugins/lsp/cmp.lua
+++ lua/bin/plugins/lsp/cmp.lua
@@ -1,98 +1,83 @@
-local cmp = require('cmp')
-local lspkind = require('lspkind')
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+local lspkind = require("lspkind")
 
 require("luasnip.loaders.from_vscode").lazy_load()
 
-cmp.setup {
-  -- As currently, i am not using any snippet manager, thus disabled it.
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  mapping = {
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),  -- Down
-    -- C-b (back) C-f (forward) for snippet placeholder navigation.
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  },
-  formatting = {
-    format = lspkind.cmp_format {
-      with_text = false,
-      menu = {
-        buffer   = "(BUF)",
-        nvim_lsp = "(LSP)",
-        path     = "(PATH)",
-        buffer_lines = '(BUFL)',
-      },
-    },
-  },
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "path" },
-    { name = 'luasnip' },
-    { name = "buffer",  keyword_length = 5 },
-    sources = {
-      {
-        name = "buffer-lines",
-        option = {
-          words = true,
-          comments = true,
-        }
-      }
-    },
-  },
-  experimental = {
-    -- ghost_text = true
-  }
-}
+cmp.setup({
+	-- As currently, i am not using any snippet manager, thus disabled it.
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = {
+		["<C-b>"] = cmp.mapping.scroll_docs(-4), -- Up
+		["<C-f>"] = cmp.mapping.scroll_docs(4), -- Down
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		}),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	},
+	formatting = {
+		fields = { "kind", "abbr", "menu" },
+		format = function(entry, vim_item)
+			local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+			local strings = vim.split(kind.kind, "%s", { trimempty = true })
+			kind.kind = " " .. (strings[1] or "") .. " "
+			kind.menu = "    (" .. (strings[2] or "") .. ")"
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-require('lspconfig')["bashls"].setup {
-  capabilities = capabilities
-}
-require('lspconfig')["lua_ls"].setup {
-  capabilities = capabilities
-}
-require('lspconfig')["html"].setup {
-  capabilities = capabilities
-}
-require('lspconfig')["tsserver"].setup {
-  capabilities = capabilities
-}
-
-require('lspconfig')["cssls"].setup {
-  capabilities = capabilities
-}
-require('lspconfig')["volar"].setup {
-  capabilities = capabilities
-}
-require('lspconfig')["eslint"].setup {
-  capabilities = capabilities
-}
+			return kind
+		end,
+		-- format = lspkind.cmp_format {
+		--   with_text = false,
+		--   menu = {
+		--     buffer   = "(BUF)",
+		--     nvim_lsp = "(LSP)",
+		--     path     = "(PATH)",
+		--     buffer_lines = '(BUFL)',
+		--   },
+		-- },
+	},
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "path" },
+		{ name = "luasnip" },
+		{ name = "buffer", keyword_length = 5 },
+		sources = {
+			{
+				name = "buffer-lines",
+				option = {
+					words = true,
+					comments = true,
+				},
+			},
+		},
+	},
+	experimental = {
+		ghost_text = true,
+	},
+})
