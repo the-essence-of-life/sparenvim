28860380c2566618aadb7336ac3cbd53bb07be19
the-essence-of-life <2193714201@qq.com>
2023-06-11 21:45:45 +0800

    v5.0(debug) fixed: formatting(change to null-ls into on-attach.)

--- lua/bin/plugins/lsp/null-ls.lua
+++ lua/bin/plugins/lsp/null-ls.lua
@@ -1,4 +1,13 @@
 local null_ls = require("null-ls")
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    filter = function(client)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  })
+end
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
   sources = {
@@ -11,4 +20,16 @@ null_ls.setup({
     null_ls.builtins.completion.tags,
     -- null_ls.builtins.completion.spell,
   },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          lsp_formatting(bufnr)
+        end,
+      })
+    end
+  end,
 })
