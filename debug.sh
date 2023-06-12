c23290117cbfe451665ea94ef3a09498e660c98c
the-essence-of-life <2193714201@qq.com>
2023-06-12 22:01:05 +0800

    5.0(debug3): fix(bugs): makefile depend two files: runs.sh and debug.sh

---
+++ debug.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# shellcheck disable=1090
+echo "Debug log:"
+nvim --headless +q
+if [ -f ~/.bashrc ]; then
+	echo "alias debug-nvim='nvim --headless +q'" >>~/.bashrc
+elif [ -f ~/.zshrc ]; then
+	echo "alias debug-nvim='nvim --headless +q'" >>~/.zshrc
+elif [ -f ~/.config/fish/config.fish ]; then
+	echo "abbr -a debug-nvim nvim --headless +q" >>~/.config/fish/config.fish
+fi
+echo "You can use debug-nvim to debug your neovim."
+echo "Please reboot your terminal to use the command."
