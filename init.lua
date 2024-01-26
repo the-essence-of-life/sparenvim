d43025c88a9cf9fd43e8dfeff7e796afa7da416f
the-essence-of-life <2193714201@qq.com>
2024-01-26 11:49:00 +0800

    v6.0 fix(loader): When user/config.lua not create,we have a checkhealth to let you create it.

--- init.lua
+++ init.lua
@@ -1,36 +1,20 @@
-local user_path = vim.fn.stdpath("config") .. "/lua/user/"
 local user_config = vim.fn.stdpath("config") .. "/lua/user/config.lua"
-if not vim.loop.fs_stat(user_path) then
-  vim.fn.system("mkdir", user_path)
-end
-  if not vim.loop.fs_stat(user_config) then
-    local file = io.open(user_config, "w+")
-    io.output(file)
-    io.write([[
-return {
-  options = {
-    enabled = true,
-  },
-  keymaps = {
-    enabled = true,
-  },
-  autocmds = {
-    enabled = true,
-    lastplace = true,
-    directory = true,
-  },
-  plugin = {
-    enabled = true,
-    mode = "plugins",
-  },
-}
-  ]])
-    io.close(file)
+if vim.loop.fs_stat(user_config) then
+  local options = require("user.config")
+  if type(options) == "table" then
+    require("spare.utils").setup(options)
   end
-local options = require("user.config")
-require("spare.utils").setup(options)
--- local function user_command(alias, cmd)
---   vim.api.nvim_create_user_command(alias, cmd, { bang = true })
--- end
-
--- user_command('SayHello', 'echo "Hello world!"')
+else
+  vim.api.nvim_echo({
+    {
+      "Welcome use spare!\n",
+      -- "WarningMsg"
+    },
+    {
+      "Press any key to start checkhealth.\n",
+      "MoreMsg"
+    },
+  }, true, {})
+  vim.fn.getchar()
+  vim.cmd("checkhealth spare.utils")
+end
