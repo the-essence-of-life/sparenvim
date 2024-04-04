f13ef8cbb397cf6db2b90f08e3224154222a6b55
the-essence-of-life <2193714201@qq.com>
2024-04-04 11:00:06 +0800

    v3.0.0 Use starter to install it.

--- init.lua
+++ init.lua
@@ -1,1 +1,14 @@
-require("spare.utils.runtime")
+vim.api.nvim_echo({
+  {
+    "This repositary should use starter.\n",
+    "ErrorMsg"
+  },
+  {
+    "Use `rm -rf ~/.config/nvim/` to remove it.\n",
+    "WarningMsg"
+  },
+  { "Press any key to exit.", "MoreMsg" }
+}, true, {})
+
+vim.fn.getchar()
+vim.cmd("quit")
